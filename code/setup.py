456d1c123d24ef03218bc56f5e27ced5ab38112b
Jonathan Mitnik <jmitnik@gmail.com>
2020-01-09 23:33:04 +0100

    Added dataset, config file and load.py

---
+++ code/setup.py
@@ -0,0 +1,11 @@
+from typing import NamedTuple
+
+class Config(NamedTuple):
+    # Path to CelebA images
+    path_to_celeba_images: str = 'data/celeba/images'
+    # Path to CelebA bounding-boxes
+    path_to_celeba_bbox_file: str = 'data/celeba/list_bbox_celeba.txt'
+    # Path to ImageNet images
+    path_to_imagenet_images: str = 'data/imagenet'
+
+config = Config()
