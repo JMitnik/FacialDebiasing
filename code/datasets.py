87d1ea391ac2a743ee3cfc86d5611a1546b75ce8
Jonathan Mitnik <jmitnik@gmail.com>
2020-01-13 13:59:55 +0100

    ataset

--- code/datasets.py
+++ code/datasets.py
@@ -1,4 +1,4 @@
-from torch.utils.data import Dataset as TorchDataset, ConcatDataset, DataLoader, random_split, Dataset
+from torch.utils.data import Dataset as TorchDataset, ConcatDataset, DataLoader, random_split, Dataset, RandomSampler
 from torch.utils.data.dataset import Subset
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
@@ -69,40 +69,38 @@ class ImagenetDataset(ImageFolder):
 
         return [self.__getitem__(idx) for idx in idxs]
 
-def split_dataset(dataset, train_size: float):
+def split_dataset(dataset, train_size: float, max_images: Optional[int] = None):
     nr_train: int = int(np.floor(train_size * len(dataset)))
     nr_valid: int = len(dataset) - nr_train
 
-    return random_split(dataset, [nr_train, nr_valid])
+    train_data, valid_data = random_split(dataset, [nr_train, nr_valid])
+
+    if max_images:
+        train_idxs = np.random.permutation(np.arange(0, len(train_data)))[:max_images]
+        train_data = Subset(train_data, train_idxs)
+
+        valid_idxs = np.random.permutation(np.arange(0, len(valid_data)))[:max_images]
+        valid_data = Subset(valid_data, valid_idxs)
+    return train_data, valid_data
 
 def train_and_valid_loaders(
     batch_size: int,
     shuffle: bool = True,
     train_size: float = 0.8,
-    max_train_images: Optional[int] = None,
-    max_valid_images: Optional[int] = None,
+    max_images: Optional[int] = None,
+    amount_faces: float = 0.5,
 ):
     # Create and concatenate multiple sources of data
     imagenet_dataset: Dataset = ImagenetDataset(config.path_to_imagenet_images)
     celeb_dataset: Dataset = CelebDataset(config.path_to_celeba_images, config.path_to_celeba_bbox_file)
 
     # Split into train and valid datasets
-    imagenet_train, imagenet_valid = split_dataset(imagenet_dataset, train_size)
-    celeb_train, celeb_valid = split_dataset(celeb_dataset, train_size)
+    celeb_train, celeb_valid = split_dataset(celeb_dataset, train_size, max_images)
+    imagenet_train, imagenet_valid = split_dataset(imagenet_dataset, train_size, max_images)
 
     # Concat the sources of data
     dataset_train: Dataset = ConcatDataset([imagenet_train, celeb_train])
     dataset_valid: Dataset = ConcatDataset([imagenet_valid, celeb_valid])
-
-    # Sample train images
-    if max_train_images:
-        nr_train_images: int = min([len(dataset_train), max_train_images])
-        dataset_train: Dataset = Subset(dataset_train, np.random.permutation(np.arange(0, nr_train_images - 1)))
-
-    # Sample valid images
-    if max_valid_images:
-        nr_valid_images: int = min([len(dataset_valid), max_valid_images])
-        dataset_valid: Dataset = Subset(dataset_valid, np.random.permutation(np.arange(0, nr_valid_images - 1)))
 
     # Define the loaders
     train_loader: DataLoader = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle)
