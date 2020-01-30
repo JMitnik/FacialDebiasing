5eea1402231bc98f227c48e118f1674eec4b2e6a
Jonathan Mitnik <JMitnik@gmail.com>
2020-01-30 20:37:26 +0100

    Create download.py

---
+++ code/download.py
@@ -0,0 +1,37 @@
+# Dependencies
+import os
+import urllib.request
+import tarfile
+
+def ensure_path(path):
+    if not os.path.exists(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    return path
+
+
+# Fetch train data
+path_to_train_data = 'data_test/train_face.h5'
+train_url = 'https://www.dropbox.com/s/l5iqduhe0gwxumq/train_face.h5?dl=1'
+
+if not os.path.exists(path_to_train_data):
+    print(f"Going to fetch training data, will place it in {path_to_train_data}.")
+    urllib.request.urlretrieve(train_url, ensure_path(path_to_temp_data))
+else:
+    print("Train data already exists.")
+
+
+# Fetch train data
+path_to_test_data = 'data_test/pbb/PBB.tar'
+test_url = 'https://www.dropbox.com/s/l0lp6qxeplumouf/PPB.tar?dl=1'
+
+if not os.path.exists(path_to_test_data):
+    print(f"Going to fetch test data, will place it in {path_to_test_data}.")
+    urllib.request.urlretrieve(test_url, ensure_path(path_to_test_data))
+
+    with tarfile.open(f"{path_to_test_data}", "r:") as tar:
+        members = [tar_mem for tar_mem in tar.getmembers()]
+        tar.extractall(os.path.dirname(path_to_test_data), members=members)
+
+else:
+    print("Test data already exists.")
