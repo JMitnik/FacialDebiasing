d6f3b1c5e0895130489265f87961a3ffc21d1fbc
Pieter <oyenspieter@gmail.com>
2020-01-31 17:53:25 +0100

    Added comments to utils

--- code/utils.py
+++ code/utils.py
@@ -39,6 +39,7 @@ def get_best_and_worst_predictions(labels, pred, device):
     return best_faces, worst_faces, best_other, worst_other
 
 def remove_frame(plt):
+    """Removes frames from a pyplot plot. """ 
     # TODO: Add annotation
     frame = plt.gca()
     for xlabel_i in frame.axes.get_xticklabels():
@@ -62,6 +63,7 @@ def concat_batches(batch_a: DatasetOutput, batch_b: DatasetOutput):
 
 
 def read_image(path_to_image):
+    """Reads an image into memory and transform to a tensor.""" 
     img: Image = Image.open(path_to_image)
 
     transforms = default_transforms()
@@ -70,12 +72,14 @@ def read_image(path_to_image):
     return img_tensor
 
 def read_flags(path_to_model):
+    """"""
     path_to_flags = f"results/{path_to_model}/flags.txt"
 
     with open(path_to_flags, 'r') as f:
         data = f.readlines()
 
 def find_face_in_subimages(model, sub_images: torch.Tensor, device: str):
+    """Finds a face in a tensor of subimages using a models' evaluation method."""
     model.eval()
 
     for images in sub_images:
@@ -96,11 +100,13 @@ def find_face_in_subimages(model, sub_images: torch.Tensor, device: str):
 
 
 def default_transforms():
+    """Transforms a transform object to a 64 by 64 tensor."""
     return transforms.Compose([
         transforms.Resize((64, 64)),
         transforms.ToTensor()
     ])
 
 def visualize_tensor(img_tensor: torch.Tensor):
+    """Visualizes a image tensor."""
     pil_transformer = transforms.ToPILImage()
     pil_transformer(img_tensor).show()
