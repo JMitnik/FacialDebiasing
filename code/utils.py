dc135c873aec37caf6ac43a35828cfae94e12665
Jonathan <JMitnik@gmail.com>
2020-01-31 19:59:07 +0100

    Final notebook

--- code/utils.py
+++ code/utils.py
@@ -38,8 +38,95 @@ def get_best_and_worst_predictions(labels, pred, device):
 
     return best_faces, worst_faces, best_other, worst_other
 
+def calculate_places(name_list, setups, w, s):
+    """Calculates the places in the final barplot."""
+    x_axis = np.arange(len(setups))
+    counter = len(name_list)-1
+
+    if (len(name_list) % 2) == 0:
+        places = []
+        times = 0
+        while counter > 0:
+            places.append(x_axis-(s/2)-s*times)
+            places.append(x_axis+(s/2)+s*times)
+
+            times += 1
+            counter -= 2
+
+    else:
+        places = [x_axis]
+        times = 1
+        while counter > 0:
+            places.append(x_axis-s*times)
+            places.append(x_axis+s*times)
+
+            times += 1
+            counter -= 2
+
+    return x_axis, sorted(places, key = lambda sub: (sub[0], sub[0]))
+
+
+def make_bar_plot(df, name_list, setups, colors=None, training_type=None, y_label="",
+                      title="", y_lim=None, y_ticks=None):
+    """Writes a bar plot for the final evaluation, based on the dataframe which stems from a results.csv."""
+    if training_type == None:
+        training_type = name_list
+    if colors == None:
+        colors = np.random.rand(len(name_list), 3)
+
+    s = 0.8/len(name_list)
+    w = s-0.02
+
+    x_axis, places = calculate_places(name_list, setups, w, s)
+
+
+    _ = plt.figure(figsize=(16, 6))
+    ax = plt.subplot(111)
+    for i in range(len(name_list)):
+        ax.bar(places[i], df.loc[df["name"].str.contains(name_list[i]), setups].mean(), label=training_type[i],
+                          yerr=df.loc[df["name"].str.contains(name_list[i]), setups].std(),
+                          color=colors[i], width=w, edgecolor="black", linewidth=2,capsize=10)
+
+
+    plt.ylabel(y_label, fontdict={"fontsize":20})
+    plt.xticks(x_axis, setups, fontsize=25)
+
+    if y_lim != None:
+        plt.ylim(y_lim[0], y_lim[1])
+
+    if y_ticks != None:
+        plt.yticks(y_ticks, fontsize=20)
+
+    plt.title(title)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.10), ncol=5, frameon=False, prop={'size': 19})
+    plt.show()
+
+def make_box_plot(df, name_list, training_type=None, colors=None, y_label="", title="", y_lim=None):
+    """Writes a box plot for the final evaluation, based on the dataframe which stems from a results.csv."""
+    if training_type == None:
+        training_type = [""] + name_list
+
+    fig = plt.figure(figsize=(16, 6))
+
+    box_plot_data=[df.loc[df["name"].str.contains(name),:]['var'] for name in name_list]
+    box = plt.boxplot(box_plot_data, patch_artist=True)
+
+
+    if y_lim != None:
+        plt.ylim(y_lim[0], y_lim[1])
+
+    plt.xticks(range(len(training_type)), training_type, fontsize=12)
+
+    if colors != None:
+        for patch, color in zip(box['boxes'], colors):
+            patch.set_facecolor(color)
+
+    plt.ylabel(y_label, fontsize=20)
+    plt.show()
+
+
 def remove_frame(plt):
-    """Removes frames from a pyplot plot. """ 
+    """Removes frames from a pyplot plot. """
     # TODO: Add annotation
     frame = plt.gca()
     for xlabel_i in frame.axes.get_xticklabels():
@@ -63,7 +150,7 @@ def concat_batches(batch_a: DatasetOutput, batch_b: DatasetOutput):
 
 
 def read_image(path_to_image):
-    """Reads an image into memory and transform to a tensor.""" 
+    """Reads an image into memory and transform to a tensor."""
     img: Image = Image.open(path_to_image)
 
     transforms = default_transforms()
